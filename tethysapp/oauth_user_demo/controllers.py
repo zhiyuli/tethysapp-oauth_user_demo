95cca6f7364ec53c67f497fa0638b32b0fb9e23e
root <root@centos66.svr.clean>
2015-11-24 13:24:04 -0500

    update

--- tethysapp/oauth_user_demo/controllers.py
+++ tethysapp/oauth_user_demo/controllers.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from social_auth.models import UserSocialAuth
 from django.conf import settings
-# from oauthlib.oauth2 import TokenExpiredError
+from oauthlib.oauth2 import TokenExpiredError
 from hs_restclient import HydroShare, HydroShareAuthOAuth2
 
 @login_required()
@@ -14,26 +14,37 @@ def home(request):
     current_user = request.user
     
     social = request.user.social_auth.get(provider='hydroshare')
-    
+    access_token_old = social.extra_data['access_token']
+    social.refresh_token()
     client_id = getattr(settings, "SOCIAL_AUTH_HYDROSHARE_KEY", "None") 
     client_secret = getattr(settings, "SOCIAL_AUTH_HYDROSHARE_SECRET", "None")
     access_token = social.extra_data['access_token']
+    
     token = {
        "access_token": access_token,
        "token_type": "Bearer",
-       "expires_in": 36000,
+       "expires_in": 10,
        "refresh_token": "123",
        "scope": "read write groups"
                  }
     auth = HydroShareAuthOAuth2(client_id, client_secret, token=token)
+    output += str(social.extra_data)+"____token_old: " + str(access_token_old) +";__ token: "+ str(access_token) + ";__ " + str(client_id) +";__  "+str(client_secret)
     try:
-    	hs = HydroShare(auth=auth)
+    	hs = HydroShare(auth=auth, hostname='playground.hydroshare.org')
     	for resource in hs.getResourceList():
           output += str(resource)
+    except TokenExpiredError as e:
+        output += str("Token Expired!")
     except:
-    	output += str("error!") 
-    
-    output += str(access_token) +"\n"+ str(client_id) +"\n"+str(client_secret)
-    context = {"output": output}
+        output += str("error!")    
 
+    abstract = 'My abstract'
+    title = 'My resource'
+    keywords = ('my keyword 1', 'my keyword 2')
+    rtype = 'GenericResource'
+    fpath = '/tmp/icon.gif'
+    resource_id = hs.createResource(rtype, title, resource_file=fpath, keywords=keywords, abstract=abstract)
+    
+    context = {"output": output, "resource_id": resource_id}
+    
     return render(request, 'oauth_user_demo/home.html', context)
